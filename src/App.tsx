1557cd6ca129b2d855a237e0b52b76358d9e57ad
Dylan Fawkes <dylanfawkes93@gmail.com>
2022-07-25 17:29:13 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dylan Fawkes</h1>
+      <div style={{ textAlign: "left" }}>
+        <span>Projects</span>
+        <ul>
+          <li>Hiscore</li>
+          <li>Tmux3270</li>
+        </ul>
+        <span>Social</span>
+        <ul>
+          <li>Twitter</li>
+          <li>LinkedIn</li>
+        </ul>
+        <span>TTRPG</span>
+        <ul>
+          <li>Pathfinder 2e: Monkey Vaults</li>
+          <li>D&D 5e: Unnamed</li>
+        </ul>
+        <span>Other</span>
+        <ul>
+          <li>Screenshots</li>
+          <li>???</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
